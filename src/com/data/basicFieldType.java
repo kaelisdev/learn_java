1b259c47507719a292e6a5ba8498097444a7aa3c
Terzi Ion <terziion@gmail.com>
2015-07-16 15:47:12 +0300

    edit data field type

--- src/com/data/basicFieldType.java
+++ src/com/data/basicFieldType.java
@@ -1,22 +1,44 @@
 package com.data;
 
+import java.util.Arrays;
+
 /**
  * Created by ion on 7/16/15.
  */
 public class basicFieldType {
 
     public  enum FieldType {
-        PASSWORD, EMAIL_ADDRES, PHONE_NUMBER, SOCIAL_SECURITY_NUMBER
+        PASSWORD, EMAIL_ADDRESS, PHONE_NUMBER, SOCIAL_SECURITY_NUMBER
     }
 
     public static void main(String[] args) {
-        FieldType field = FieldType.EMAIL_ADDRES;
+        FieldType field = FieldType.EMAIL_ADDRESS;
+
         System.out.println("field.name(): " + field.name());
-        System.out.println("test row");
-        System.out.println("test row");
-        System.out.println("test row");
-        System.out.println("test row");
-        System.out.println("test row");
-        System.out.println("test row");
+        System.out.println("field.ordinal(): " + field.ordinal());
+        System.out.println("field.toString(): " + field.toString());
+        System.out.println("field.isEqual(EMAIL_ADDRESS): " +
+                field.equals(FieldType.EMAIL_ADDRESS));
+        System.out.println("field.isEqual(\"EMAIL_ADDRESS\"'): " + field.equals("EMAIL_ADDRESS"));
+
+        System.out.println("field == EMAIL_ADDRESS: " + (field == FieldType.EMAIL_ADDRESS));
+        // Won't compile - illustrates type safety of enum
+        // System.out.println("field == \"EMAIL_ADDRESS\": " + (field == "EMAIL_ADDRESS"));
+
+        System.out.println("field.compareTo(EMAIL_ADDRESS): " +
+                field.compareTo(FieldType.EMAIL_ADDRESS));
+        System.out.println("field.compareTo(PASSWORD): " + field.compareTo(FieldType.PASSWORD));
+
+        System.out.println("field.valueOf(\"EMAIL_ADDRESS\"): " + field.valueOf("EMAIL_ADDRESS"));
+
+        try {
+            System.out.print("field.valueOf(\"email_address\"): ");
+            System.out.println(FieldType.valueOf("email_address"));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.toString());
+        }
+
+        System.out.println("FieldType.values(): " + Arrays.toString(FieldType.values()));
+
     }
 }
